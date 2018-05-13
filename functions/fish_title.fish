07b92b32c32026b5f90b094aed9774804621af6d
Wolfgang Schnerring <wosc@wosc.de>
2018-05-13 15:00:57 +0200

    Support tmux

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,5 +1,7 @@
 function fish_title
     set -l hostname (hostname)
     set -l cwd (basename (prompt_pwd))
-    echo "$hostname:$cwd/"
+    set -l title "$hostname:$cwd/"
+    tmux rename-window $title
+    echo "$title"
 end
