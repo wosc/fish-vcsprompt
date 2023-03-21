d20d8f8309a199b14b965268b9c6e6bf68c52a6a
Wolfgang Schnerring <wosc@wosc.de>
2023-03-21 21:07:03 +0100

    I'm not actually using tmux

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -2,6 +2,6 @@ function fish_title
     set -l myhostname (hostname)
     set -l cwd (basename (prompt_pwd))
     set -l title "$myhostname:$cwd/"
-    tmux rename-window $title
+    # tmux rename-window $title
     echo "$title"
 end
