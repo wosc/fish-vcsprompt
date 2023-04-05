7364ef701e61a0b822060d94e4c78ebd11d10940
Wolfgang Schnerring <wosc@wosc.de>
2023-04-05 16:14:18 +0200

    Inline the one function git_branch_name we used from fisherman/git_util
    The package was dissolved in https://github.com/jorgebucaran/fisher/issues/446

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -75,3 +75,19 @@ function fish_prompt
 
     echo -n -s $host_color $myhostname ":" $directory_color $cwd $vcs_info $prompt_char $normal_color
 end
+
+function git_branch_name -d "Get the name of the current Git branch, tag or sha1"
+    set -l branch_name (command git symbolic-ref --short HEAD ^/dev/null)
+
+    if test -z "$branch_name"
+        set -l tag_name (command git describe --tags --exact-match HEAD ^ /dev/null)
+
+        if test -z "$tag_name"
+            command git rev-parse --short HEAD ^ /dev/null
+        else
+            printf "%s\n" "$tag_name"
+        end
+    else
+        printf "%s\n" "$branch_name"
+    end
+end
