44fd33bc852d39f9f1a0f95beaa47cfad073a38e
Wolfgang Schnerring <wosc@wosc.de>
2023-04-05 16:31:28 +0200

    Update to non-deprecated fish syntax

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -77,13 +77,13 @@ function fish_prompt
 end
 
 function git_branch_name -d "Get the name of the current Git branch, tag or sha1"
-    set -l branch_name (command git symbolic-ref --short HEAD ^/dev/null)
+    set -l branch_name (command git symbolic-ref --short HEAD 2>/dev/null)
 
     if test -z "$branch_name"
-        set -l tag_name (command git describe --tags --exact-match HEAD ^ /dev/null)
+        set -l tag_name (command git describe --tags --exact-match HEAD 2>/dev/null)
 
         if test -z "$tag_name"
-            command git rev-parse --short HEAD ^ /dev/null
+            command git rev-parse --short HEAD 2>/dev/null
         else
             printf "%s\n" "$tag_name"
         end
